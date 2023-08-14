e6fff6bb43ca69aea52e0f135c2b5529506024b3
Rangordo <Nathan_Roskamp@yahoo.de>
2023-08-14 12:02:03 +0200

    Update kata_1.go

--- kata_1.go
+++ kata_1.go
@@ -20,41 +20,43 @@ func (s *Item) Push(val interface{}) {
 
 // function returns the latest index's element and delets the latest index afterwards
 func (s *Item) Pop() (interface{}, bool) {
+	i := 0
+	b := false
 	if s.IsEmpty() == false {
-		i := len(s.Item) - 1
-		e := s.Item[i]
-		s.Item = s.Item[:i]
-		return e, true
-	} else {
-		return 0, false
+		i = len(s.Item) - 1
+		b = true
 	}
+	e := s.Item[i]
+	s.Item = s.Item[:i]
+	return e, b
 }
 
 // function returns the latest index's element
 func (s *Item) Peek() (interface{}, bool) {
+	i := 0
+	b := false
 	if s.IsEmpty() == false {
-		i := len(s.Item) - 1
-		e := s.Item[i]
-		return e, true
-	} else {
-		return 0, false
+		i = len(s.Item) - 1
+		b = true
 	}
+	e := s.Item[i]
+	return e, b
 }
 
 // function indicates whether the input is empty
 func (s *Item) IsEmpty() bool {
+	b := false
 	if len(s.Item) == 0 {
-		return true
-	} else {
-		return false
+		b = true
 	}
+	return b
 }
 
 func main() {
 	Item := NewStack()
 	Item.Push(5)
 	Item.Push(10)
-	Item.Push("text")
+	Item.Push(15)
 	val, _ := Item.Peek()
 	fmt.Println(val)
 	val, _ = Item.Pop()
